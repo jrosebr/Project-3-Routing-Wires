c84c73d7bb2ad58139c850cddc2376a3d81c8129
jrosebr <jrosebr@iu.edu>
2024-04-15 10:19:10 -0400

    started the project

--- src/Routing.java
+++ src/Routing.java
@@ -12,12 +12,70 @@ public class Routing {
     public static ArrayList<Wire>
     findPaths(Board board, ArrayList<Endpoints> goals)
     {
-        return null;  // replace this line with your code
+        ArrayList<Wire> paths = new ArrayList<>();
+
+        //Iterate through each pair of endpoint
+        for (int i = 0; i < goals.size(); ++i)
+        {
+            for (int j = i; j < goals.size(); ++j)
+            {
+                //Find path between pair
+                Wire path = findPath(board, goals.get(i).start, goals.get(j).end);
+                if (path != null)
+                {
+                    paths.add(path);
+                    board.placeWire(path); //Create the wire on the board
+                }
+            }
+        }
+
+        return paths;
     }
 
-    private void bfs(Board board, Coord start, Map<Coord, Boolean> visited, Map<Coord, Coord> parent)
+    private static Wire findPath(Board board, Coord start, Coord end)
     {
+        Map<Coord, Coord> parent = new HashMap<>();
+        Queue<Coord> queue = new LinkedList<>();
+        Map<Coord, Boolean> visited = new HashMap<>();
 
+        queue.offer(start);
+        visited.put(start, true);
+
+        while (!queue.isEmpty())
+        {
+            Coord current = queue.poll();
+
+            if (current.equals(end))
+                return constructPath(parent, start, end);
+
+            for (Coord neighbor : board.adj(current))
+            {
+                if (!visited.containsKey(neighbor) && !board.isObstacle(neighbor))
+                {
+                    queue.offer(neighbor);
+                    visited.put(neighbor, true);
+                    parent.put(neighbor, current);
+                }
+            }
+        }
+
+        return null; //No path found
     }
 
+    private static Wire constructPath(Map<Coord, Coord> parent, Coord start, Coord end)
+    {
+        ArrayList<Coord> pathPoints = new ArrayList<>();
+        Coord current = end;
+
+        while (!current.equals(start))
+        {
+            pathPoints.add(current);
+            current = parent.get(current);
+        }
+        pathPoints.add(start);
+
+        Collections.reverse(pathPoints);
+        Wire path = new Wire(1, pathPoints);
+        return path;
+    }
 }
